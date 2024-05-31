2a581604866e927d3951c5253bb4d3b87beba4ca
Ben Manley <benjamintmanley@gmail.com>
2024-05-31 08:41:34 -0500

    add code for M-M association

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
   path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
   path('toys/', views.ToyList.as_view(), name='toy-index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
-  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete')
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+  path('cats/<int:cat_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
+  
 ]
